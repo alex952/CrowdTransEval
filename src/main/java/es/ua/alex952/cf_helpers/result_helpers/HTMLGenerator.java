56944ca3d370c0eddae852f4a776134071387f39
alex952 <alex952.anf5@gmail.com>
2012-06-27 16:40:14 +0200

    Last changes on HTMLGenerator

--- src/main/java/es/ua/alex952/cf_helpers/result_helpers/HTMLGenerator.java
+++ src/main/java/es/ua/alex952/cf_helpers/result_helpers/HTMLGenerator.java
@@ -1,20 +1,19 @@
 /**
- * CrowdTransEval, a toolkit for evaluating machine translation
- * system by using crowdsourcing.
- * Copyright (C) 2012 Alejandro Navarro Fulleda <anf5@alu.ua.es>
+ * CrowdTransEval, a toolkit for evaluating machine translation system by using
+ * crowdsourcing. Copyright (C) 2012 Alejandro Navarro Fulleda <anf5@alu.ua.es>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package es.ua.alex952.cf_helpers.result_helpers;
 
@@ -53,7 +52,7 @@ public class HTMLGenerator {
 
 	/**
 	 * Entrance point of the class (Singleton pattern).
-	 * 
+	 *
 	 * @return Unique instance of the class
 	 */
 	public static HTMLGenerator getGenerator() {
@@ -111,48 +110,48 @@ public class HTMLGenerator {
 	}
 
 	/**
-	 * Generates charts representing the Kappa average for
-	 * each service (fluency and adequacy).
-	 * 
+	 * Generates charts representing the Kappa average for each service (fluency
+	 * and adequacy).
+	 *
 	 * @param raters Hashmap of pairs of raters
 	 */
 	public void generateKappaAverageCharts(HashMap<String, HashMap<String, KappaRaters>> raters) {
 		String chart = "var adequacy = new google.visualization.DataTable();\n"
-					+ "adequacy.addColumn(\"string\", \"Service\");\n"
-					+ "adequacy.addColumn(\"number\", \"Adequacy\");\n"
-					+ "var fluency = new google.visualization.DataTable();\n"
-					+ "fluency.addColumn(\"string\", \"Service\");\n"
-					+ "fluency.addColumn(\"number\", \"Fluency\");\n";
-		
+				+ "adequacy.addColumn(\"string\", \"Service\");\n"
+				+ "adequacy.addColumn(\"number\", \"Adequacy\");\n"
+				+ "var fluency = new google.visualization.DataTable();\n"
+				+ "fluency.addColumn(\"string\", \"Service\");\n"
+				+ "fluency.addColumn(\"number\", \"Fluency\");\n";
+
 		String adata = "";
 		String fdata = "";
-		
+
 		Set<String> services = (Set<String>) raters.keySet();
 		int scount = 0;
-		
-		for(String service: services) {
+
+		for (String service : services) {
 			HashMap<String, KappaRaters> serviceRatters = raters.get(service);
-			
+
 			double f = 0.0;
 			double a = 0.0;
-			
-			for(String key: serviceRatters.keySet()) {
+
+			for (String key : serviceRatters.keySet()) {
 				f += serviceRatters.get(key).getFluencyKappa();
 				a += serviceRatters.get(key).getAdequacyKappa();
 			}
-			
+
 			f /= serviceRatters.size();
 			a /= serviceRatters.size();
-			
+
 			adata += "['" + service + "', " + a + "]";
 			fdata += "['" + service + "', " + f + "]";
-			
+
 			if (scount++ < services.size() - 1) {
 				adata += ",";
 				fdata += ",";
 			}
 		}
-		
+
 		chart += "adequacy.addRows([\n"
 				+ adata
 				+ "]);\n";
@@ -173,54 +172,54 @@ public class HTMLGenerator {
 		chart += "var chartF = new google.visualization.ColumnChart(document.getElementById('kappa_average_fluency'));\n";
 		chart += "chartA.draw(adequacy, optionsA);\n";
 		chart += "chartF.draw(fluency, optionsF);\n";
-		
+
 		head += chart;
 		body += "<h2>Kappa averages by service</h2>\n"
 				+ "<div id='kappa_average_adequacy'></div>\n"
 				+ "<div id='kappa_average_fluency'></div>\n";
 	}
-	
+
 	public void generateScoreCharts(JSONParams judgments, Service[] services) {
 		String chart = "var adequacy = new google.visualization.DataTable();\n"
-					+ "adequacy.addColumn(\"string\", \"Service\");\n"
-					+ "adequacy.addColumn(\"number\", \"Adequacy\");\n"
-					+ "var fluency = new google.visualization.DataTable();\n"
-					+ "fluency.addColumn(\"string\", \"Service\");\n"
-					+ "fluency.addColumn(\"number\", \"Fluency\");\n";
-		
+				+ "adequacy.addColumn(\"string\", \"Service\");\n"
+				+ "adequacy.addColumn(\"number\", \"Adequacy\");\n"
+				+ "var fluency = new google.visualization.DataTable();\n"
+				+ "fluency.addColumn(\"string\", \"Service\");\n"
+				+ "fluency.addColumn(\"number\", \"Fluency\");\n";
+
 		String adata = "";
 		String fdata = "";
-		
-        Double[] acumFluencyServices = new Double[services.length];
-        Double[] acumAdequacyServices = new Double[services.length];
-        Iterator<String> unitsIterator = judgments.getKeySet();
-        while (unitsIterator.hasNext()) {
-            String u = unitsIterator.next();
-            JSONParams unit = judgments.getObject(u);
 
-            for(int i = 0; i < services.length; i++) {
-                String service = services[i].getName();
-                acumFluencyServices[i] = Double.parseDouble(unit.getObject("fluency_" + service).getProperty("avg"));
-                acumAdequacyServices[i] = Double.parseDouble(unit.getObject("adequacy_" + service).getProperty("avg"));
-            }
-        }
+		Double[] acumFluencyServices = new Double[services.length];
+		Double[] acumAdequacyServices = new Double[services.length];
+		Iterator<String> unitsIterator = judgments.getKeySet();
+		while (unitsIterator.hasNext()) {
+			String u = unitsIterator.next();
+			JSONParams unit = judgments.getObject(u);
 
-        for(int i = 0; i < services.length; i++) {
-            acumFluencyServices[i] /= judgments.size();
-            acumAdequacyServices[i] /= judgments.size();
-        }
+			for (int i = 0; i < services.length; i++) {
+				String service = services[i].getName();
+				acumFluencyServices[i] = Double.parseDouble(unit.getObject("fluency_" + service).getProperty("avg"));
+				acumAdequacyServices[i] = Double.parseDouble(unit.getObject("adequacy_" + service).getProperty("avg"));
+			}
+		}
 
-        for(int i = 0; i < services.length; i++) {
-            String service = services[i].getName();
+		for (int i = 0; i < services.length; i++) {
+			acumFluencyServices[i] /= judgments.size();
+			acumAdequacyServices[i] /= judgments.size();
+		}
 
-            adata += "['" + service + "', " + acumAdequacyServices[i] + "]";
-            fdata += "['" + service + "', " + acumFluencyServices[i] + "]";
+		for (int i = 0; i < services.length; i++) {
+			String service = services[i].getName();
+
+			adata += "['" + service + "', " + acumAdequacyServices[i] + "]";
+			fdata += "['" + service + "', " + acumFluencyServices[i] + "]";
 
 			if (i < services.length - 1) {
 				adata += ",";
 				fdata += ",";
 			}
-        }
+		}
 
 		chart += "adequacy.addRows([\n"
 				+ adata
@@ -242,79 +241,81 @@ public class HTMLGenerator {
 		chart += "var chartF = new google.visualization.ColumnChart(document.getElementById('score_average_fluency'));\n";
 		chart += "chartA.draw(adequacy, optionsA);\n";
 		chart += "chartF.draw(fluency, optionsF);\n";
-		
+
 		head += chart;
 		body += "<h2>Adequacy/Fluency averages by service</h2>\n"
 				+ "<div id='score_average_adequacy'></div>\n"
 				+ "<div id='score_average_fluency'></div>\n";
 	}
-	
+
 	/**
-	 * Generates a table for each service containing the
-	 * info about factor Kappa calculated every pair of raters.
-	 * 
+	 * Generates a table for each service containing the info about factor Kappa
+	 * calculated every pair of raters.
+	 *
 	 * @param raters Hashmap of pairs of raters
 	 */
 	public void generateKappaTable(HashMap<String, HashMap<String, KappaRaters>> raters) {
 
-		for(String service: raters.keySet()) {
+		for (String service : raters.keySet()) {
 			HashMap<String, KappaRaters> serviceRatters = raters.get(service);
-			
+
 			body += "<h2>Kappa results for service " + service + "</h2>\n"
-					+ "<div id=\"kappa_fluency_" + service +  "\"></div>\n"
-					+ "<div id=\"kappa_adequacy_" + service +  "\"></div>\n";
-			
+					+ "<h3>Fluency kappa</h3>\n"
+					+ "<div id=\"kappa_fluency_" + service + "\"></div>\n"
+					+ "<h3>Adequacy kappa</h3>\n"
+					+ "<div id=\"kappa_adequacy_" + service + "\"></div>\n";
+
 			HashSet<Integer> leftPart = new HashSet<Integer>();
 			HashSet<Integer> rightPart = new HashSet<Integer>();
-			for(String key: serviceRatters.keySet()) {
+			for (String key : serviceRatters.keySet()) {
 				KappaRaters rater = serviceRatters.get(key);
 				leftPart.add(rater.getRaterA());
 				rightPart.add(rater.getRaterB());
 			}
-			
+
 			String dataFluency = "var dataFluency = google.visualization.arrayToDataTable([\n"
-									+ "[ ' ', ";
-			
+					+ "[ ' ', ";
+
 			String dataAdequacy = "var dataAdequacy = google.visualization.arrayToDataTable([\n"
-									+ "[ ' ', ";
+					+ "[ ' ', ";
 			int count = 0;
-			
-			for (Integer right: rightPart) {
+
+			for (Integer right : rightPart) {
 				dataFluency += "'" + right.toString() + "'";
 				dataAdequacy += "'" + right.toString() + "'";
-				
+
 				if (count++ < rightPart.size() - 1) {
 					dataFluency += ", ";
 					dataAdequacy += ", ";
 				}
 			}
-			
+
 			dataFluency += "],\n";
 			dataAdequacy += "],\n";
 			int countl = 0;
 			int countr = 0;
-			
-			for (Integer left: leftPart) {
+
+			for (Integer left : leftPart) {
 				dataFluency += "[ '" + left + "', ";
 				dataAdequacy += "[ '" + left + "', ";
-				
-				for(Integer right: rightPart) {
-					if(serviceRatters.containsKey(left + "/" + right)) {
+
+				for (Integer right : rightPart) {
+					if (serviceRatters.containsKey(left + "/" + right)) {
 						KappaRaters k = serviceRatters.get(left + "/" + right);
-						
+
 						dataFluency += "'" + k.getFluencyKappa() + "'";
 						dataAdequacy += "'" + k.getAdequacyKappa() + "'";
 					} else {
 						dataFluency += "false";
 						dataAdequacy += "false";
 					}
-					
-					if (countr++ < rightPart.size()- 1) {
+
+					if (countr++ < rightPart.size() - 1) {
 						dataFluency += ", ";
 						dataAdequacy += ", ";
 					}
 				}
-				
+
 				dataFluency += "]";
 				dataAdequacy += "]";
 				if (countl++ < rightPart.size() - 1) {
@@ -324,10 +325,10 @@ public class HTMLGenerator {
 				dataFluency += "\n";
 				dataAdequacy += "\n";
 			}
-			
+
 			dataFluency += "]);\n";
 			dataAdequacy += "]);\n";
-			
+
 			head += dataAdequacy + dataFluency
 					+ "visualizationFluency = new google.visualization.Table(document.getElementById('kappa_fluency_" + service + "'));\n"
 					+ "visualizationAdequacy = new google.visualization.Table(document.getElementById('kappa_adequacy_" + service + "'));\n"
@@ -336,16 +337,15 @@ public class HTMLGenerator {
 		}
 	}
 
-	/*public String generateHTML(HashMap<String, HashMap<String, KappaRaters>> raters) {
-		this.generateSurrondings();
-		return this.htmlGenerated;
-	}*/
-
+	/*
+	 * public String generateHTML(HashMap<String, HashMap<String, KappaRaters>>
+	 * raters) { this.generateSurrondings(); return this.htmlGenerated;
+	}
+	 */
 	/**
-	 * Returns the html generated by the above procedures
-	 * along with the headers and extra tags for a complete
-	 * html output.
-	 * 
+	 * Returns the html generated by the above procedures along with the headers
+	 * and extra tags for a complete html output.
+	 *
 	 * @return Complete HTML file (String) with all the contente generated
 	 */
 	public String getHTML() {
@@ -372,22 +372,228 @@ public class HTMLGenerator {
 				+ "google.load(\"visualization\", \"1\", {packages:[\"corechart\", \"table\"]});\n"
 				+ "google.setOnLoadCallback(drawCharts);\n"
 				+ "function drawCharts() {\n";
-				
+
 
 		this.headBottom = "}\n"
-						+ "</script>\n"
-						+ "</head>\n";
+				+ "</script>\n"
+				+ "</head>\n";
 
 		this.bodyTop = "<body>\n";
 		this.bodyBottom = "</body>\n</html>";
 	}
-	
+
 	/**
 	 * See {@link HTMLGenerator#getHTML()}
-	 * 
-	 * @return 
+	 *
+	 * @return
 	 */
 	public String toString() {
 		return this.getHTML();
+	}
+
+	public static void main(String[] args) {
+		HTMLGenerator gen = HTMLGenerator.getGenerator();
+
+		HashMap<String, HashMap<String, KappaRaters>> rater = new HashMap<String, HashMap<String, KappaRaters>>();
+		rater.put("google", new HashMap<String, KappaRaters>());
+		rater.put("bing", new HashMap<String, KappaRaters>());
+
+		HashMap<String, KappaRaters> googleRaters = rater.get("google");
+		KappaRaters g12 = new KappaRaters(1, 2);
+		KappaRaters g13 = new KappaRaters(1, 3);
+
+		g12.addAdequacy(2, 1);
+		g12.addAdequacy(3, 4);
+		g12.addAdequacy(4, 4);
+		g12.addAdequacy(4, 4);
+		g12.addAdequacy(4, 1);
+		g12.addAdequacy(4, 2);
+		g12.addAdequacy(4, 2);
+		g12.addAdequacy(4, 2);
+		g12.addAdequacy(0, 2);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 0);
+		g12.addAdequacy(0, 4);
+		g12.addAdequacy(0, 2);
+		g12.addAdequacy(0, 2);
+		g12.addAdequacy(0, 2);
+		g12.addAdequacy(0, 2);
+		g12.addAdequacy(4, 2);
+		g12.addAdequacy(3, 4);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(1, 2);
+		g12.addAdequacy(2, 4);
+		g12.addAdequacy(2, 1);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(5, 5);
+		g12.addAdequacy(3, 4);
+		g12.addAdequacy(3, 4);
+		
+		g12.addFluency(2, 1);
+		g12.addFluency(3, 4);
+		g12.addFluency(4, 4);
+		g12.addFluency(4, 4);
+		g12.addFluency(4, 1);
+		g12.addFluency(4, 2);
+		g12.addFluency(4, 2);
+		g12.addFluency(4, 2);
+		g12.addFluency(0, 2);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 0);
+		g12.addFluency(0, 4);
+		g12.addFluency(0, 2);
+		g12.addFluency(0, 2);
+		g12.addFluency(0, 2);
+		g12.addFluency(0, 2);
+		g12.addFluency(4, 2);
+		g12.addFluency(3, 4);
+		g12.addFluency(1, 2);
+		g12.addFluency(1, 2);
+		g12.addFluency(1, 2);
+		g12.addFluency(1, 2);
+		g12.addFluency(1, 2);
+		g12.addFluency(1, 2);
+		g12.addFluency(2, 4);
+		g12.addFluency(2, 1);
+		g12.addFluency(5, 5);
+		g12.addFluency(5, 5);
+		g12.addFluency(5, 5);
+		g12.addFluency(5, 5);
+		g12.addFluency(5, 5);
+		g12.addFluency(5, 5);
+		g12.addFluency(3, 4);
+		g12.addFluency(3, 4);
+		
+		g13.addAdequacy(2, 1);
+		g13.addAdequacy(3, 4);
+		g13.addAdequacy(4, 4);
+		g13.addAdequacy(4, 4);
+		g13.addAdequacy(4, 1);
+		g13.addAdequacy(4, 2);
+		g13.addAdequacy(4, 2);
+		g13.addAdequacy(4, 2);
+		g13.addAdequacy(0, 2);
+		g13.addAdequacy(0, 0);
+		g13.addAdequacy(0, 0);
+		g13.addAdequacy(0, 0);
+		g13.addAdequacy(0, 0);
+		g13.addAdequacy(0, 0);
+		g13.addAdequacy(0, 4);
+		g13.addAdequacy(0, 2);
+		g13.addAdequacy(0, 2);
+		g13.addAdequacy(0, 2);
+		g13.addAdequacy(0, 2);
+		g13.addAdequacy(4, 2);
+		g13.addAdequacy(3, 4);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(1, 2);
+		g13.addAdequacy(2, 4);
+		g13.addAdequacy(2, 1);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(5, 5);
+		g13.addAdequacy(3, 4);
+		g13.addAdequacy(3, 4);
+		
+		g13.addFluency(2, 1);
+		g13.addFluency(3, 4);
+		g13.addFluency(4, 4);
+		g13.addFluency(4, 4);
+		g13.addFluency(4, 1);
+		g13.addFluency(4, 2);
+		g13.addFluency(4, 2);
+		g13.addFluency(4, 2);
+		g13.addFluency(0, 2);
+		g13.addFluency(0, 0);
+		g13.addFluency(0, 0);
+		g13.addFluency(0, 0);
+		g13.addFluency(0, 0);
+		g13.addFluency(0, 0);
+		g13.addFluency(0, 4);
+		g13.addFluency(0, 2);
+		g13.addFluency(0, 2);
+		g13.addFluency(0, 2);
+		g13.addFluency(0, 2);
+		g13.addFluency(4, 2);
+		g13.addFluency(3, 4);
+		g13.addFluency(1, 2);
+		g13.addFluency(1, 2);
+		g13.addFluency(1, 2);
+		g13.addFluency(1, 2);
+		g13.addFluency(1, 2);
+		g13.addFluency(1, 2);
+		g13.addFluency(2, 4);
+		g13.addFluency(2, 1);
+		g13.addFluency(5, 5);
+		g13.addFluency(5, 5);
+		g13.addFluency(5, 5);
+		g13.addFluency(5, 5);
+		g13.addFluency(5, 5);
+		g13.addFluency(5, 5);
+		g13.addFluency(3, 4);
+		g13.addFluency(3, 4);
+
+
+		googleRaters.put("1/2", g12);
+		//googleRaters.put("1/3", g13);
+
+		HashMap<String, KappaRaters> bingRaters = rater.get("bing");
+		//bingRaters.put("1/2", g12);
+		bingRaters.put("1/3", g13);
+
+		gen.generateKappaTable(rater);
+		gen.generateKappaAverageCharts(rater);
+
+		String html = gen.getHTML();
+
+
+		try {
+			FileOutputStream fos = new FileOutputStream(new File("debug.html"));
+			fos.write(html.getBytes());
+			fos.close();
+			Desktop.getDesktop().open(new File("debug.html"));
+		} catch (IOException e) {
+		}
 	}
 }
